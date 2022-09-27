1dc59528b5bdbeac3d7631c1dcfd328325d56039
lamsoc <tranvinhlam01@gmail.com>
2022-09-27 18:59:41 +0700

    feat: build home_ontap and detail_ontap

---
+++ apptracnghiem/lib/view/ontap/detail_ontap.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Detail_Ontap extends StatelessWidget {
+  const Detail_Ontap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          }, 
+          icon: Icon(Icons.arrow_back)
+        ),
+        title: Text(
+          "Ôn Tập Luật Giao thông đường bộ",
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      body: buildBody(),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  buildBody() {
+    return Container(
+      child: Column(
+        children: [
+          buildQuestion(),
+          SizedBox(height: 10),
+          buildListAnswer()
+        ],
+      ),
+    );
+  }
+
+  buildQuestion() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Câu 1/60",
+            style: TextStyle(
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          SizedBox(height: 10),
+          Text("Thuyền trưởng, người lái phương tiện khi điều khiển phương tiện hoạt động trên đường thuỷ nội địa phải tuân theo quy định:")
+        ],
+      ),
+    );
+  }
+
+  buildListAnswer() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      height: 400,
+      child: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) {
+          return buildAnswer();
+        }
+      ),
+    );
+  }
+
+  buildAnswer() {
+    return Container(
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          CircleAvatar(
+            child: Text("1"),
+            backgroundColor: Colors.blue,
+          ),
+          SizedBox(width: 20),
+          Flexible(
+            child: Text(
+              "Quy tắc giao thông vào báo hiệu đường thuỷ nội địa"
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildBottomNavigationBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 224, 221, 221)
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 35),
+      height: 60,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            child: CircleAvatar(
+              child: Icon(Icons.arrow_back),
+            ),
+            onTap: () {
+
+            },
+          ),
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.arrow_drop_up)
+          ),
+          InkWell(
+            child: CircleAvatar(
+              child: Icon(Icons.arrow_forward),
+            ),
+            onTap: () {
+              
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- apptracnghiem/lib/view/ontap/home_ontap.dart
+++ apptracnghiem/lib/view/ontap/home_ontap.dart
@@ -1,3 +1,4 @@
+import 'package:apptracnghiem/view/ontap/detail_ontap.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -6,8 +7,12 @@ import 'package:flutter/src/widgets/framework.dart';
 class Home_OnTap extends StatelessWidget {
   const Home_OnTap({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -16,58 +21,93 @@ class Home_OnTap extends StatelessWidget {
         ),
         title: Text("Ôn Tập"),
       ),
-      body: buildBody(),
+      body: buildBody(size),
     );
+
   }
 
-  buildBody() {
+  buildBody(var size) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.pink
       ),
-      padding: EdgeInsets.only(top: 5, left: 15, right: 15),
+      padding: EdgeInsets.only(top: 10, left: 20, right: 20),
       child: ListView(
         children: [
-          buildItem()
+          buildListItem(size)
         ],
       ),
     );
   }
 
-  buildItem() {
+  buildListItem(var size) {
     return Container(
-      padding: EdgeInsets.only(top: 5, bottom: 5, left: 10, right: 10),
+      height: size.height*.80,
+      child: GridView.builder(
+        itemCount: 6,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1, 
+          mainAxisSpacing: 15,
+          crossAxisSpacing: 15,
+          childAspectRatio: 4.4
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return buildItem(size, context);
+        },
+      ),
+    );
+  }
+
+  buildItem(var size, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 15),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(5)
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             decoration: BoxDecoration(
               border: Border.all(color: Color.fromARGB(255, 40, 43, 201)),
             ),
-            padding: EdgeInsets.all(5),
+            padding: EdgeInsets.all(10),
             child: Icon(Icons.abc),
           ),
-          SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                "Luật Giao thông đường thuỷ nội địa",
-                style: TextStyle(
-                  color: Colors.blue
+          SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Text(
+                    "Luật Giao thông đường thuỷ nội địa",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 16
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Detail_Ontap()));
+                  },
                 ),
-              ),
-              Text(
-                "60 câu",
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold
+                const Text(
+                  "60 câu",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16
+                  ),
                 ),
-              )
-            ]
+                const Divider(
+                  color: Colors.yellow,
+                  height: 20,
+                  thickness: 3,
+                ),
+              ]
+            ),
           )
         ],
       ),
